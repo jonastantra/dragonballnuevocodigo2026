54c432fea6f7ddb100986e1fd60f44d576e9c387
jonastantra <jonastantra@gmail.com>
2026-05-20 10:30:09 -0600

    Update Dragon Ball popup ad script

--- components/PopupAds.js
+++ components/PopupAds.js
@@ -7,7 +7,7 @@ export default function PopupAds() {
   return (
     <>
       <Script id="popup-cdn4ads" strategy="lazyOnload" data-cfasync="false">
-        {`(function(){var q=window,p="e3db55bb7c8a0626c7d042e65ea3dc5e",c=[["siteId",254+595+842-439*301+5087948],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],a=["d3d3LmNkbjRhZHMuY29tL3VlcXVhbGl6ZS5taW4uY3Nz","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvdm9uS3cvd2pxdWVyeS5maWxldXBsb2FkLm1pbi5qcw=="],o=-1,e,k,v=function(){clearTimeout(k);o++;if(a[o]&&!(1805213061000<(new Date).getTime()&&1<o)){e=q.document.createElement("script");e.type="text/javascript";e.async=!0;var g=q.document.getElementsByTagName("script")[0];e.src="https://"+atob(a[o]);e.crossOrigin="anonymous";e.onerror=v;e.onload=function(){clearTimeout(k);q[p.slice(0,16)+p.slice(0,16)]||v()};k=setTimeout(v,5E3);g.parentNode.insertBefore(e,g)}};if(!q[p]){try{Object.freeze(q[p]=c)}catch(e){}v()}})();`}
+        {`(function(){var l=window,d="e3db55bb7c8a0626c7d042e65ea3dc5e",s=[["siteId",909-624+646+4956569],["minBid",0],["popundersPerIP","0:8"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],h=["d3d3LmNkbjRhZHMuY29tL2RlcXVhbGl6ZS5taW4uY3Nz","ZDNnNW92Zm5nanc5YncuY2xvdWRmcm9udC5uZXQvRnlZL2dqcXVlcnkuZmlsZXVwbG9hZC5taW4uanM="],x=-1,b,g,p=function(){clearTimeout(g);x++;if(h[x]&&!(1805213774000<(new Date).getTime()&&1<x)){b=l.document.createElement("script");b.type="text/javascript";b.async=!0;var t=l.document.getElementsByTagName("script")[0];b.src="https://"+atob(h[x]);b.crossOrigin="anonymous";b.onerror=p;b.onload=function(){clearTimeout(g);l[d.slice(0,16)+d.slice(0,16)]||p()};g=setTimeout(p,5E3);t.parentNode.insertBefore(b,t)}};if(!l[d]){try{Object.freeze(l[d]=s)}catch(e){}p()}})();`}
       </Script>
 
       <Script id="popup-popcash" strategy="lazyOnload">
@@ -16,4 +16,3 @@ export default function PopupAds() {
     </>
   );
 }
-
